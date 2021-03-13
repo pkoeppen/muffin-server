4c25569faf5d0293906220ca17200fc30e69d4c5
Peter Koeppen <pkoeppen@lenovo.com>
2021-03-13 09:03:40 -0500

    feat: added pm2 ecosystem file

---
+++ ecosystem.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  apps: [
+    {
+      name: 'muffin-server',
+      instances: 1,
+      maxMemoryRestart: '512M',
+      script: 'node',
+      args: './src/index.js',
+    },
+  ],
+};
